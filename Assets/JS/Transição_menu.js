dda9337662d0efe0e3d8490ffdb80d552771447a
Henrickdamas <126334924+Henrickdamas@users.noreply.github.com>
2023-05-06 21:12:04 -0300

    Update Transição_menu.js

--- Assets/JS/Transição_menu.js
+++ Assets/JS/Transição_menu.js
@@ -1,6 +1,17 @@
 window.addEventListener("scroll", function (){
-    let header = document.querySelector('.header')
+    let header = document.querySelector('.conteiner-menu')
     header.classList.toggle('transition',window.scrollY > 10) 
 
 })
 
+
+function menuShow() {
+    let menuMobile = document.querySelector('.js-menu-mobile');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+    }
+}
